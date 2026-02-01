1e8b5117e28e6ddddf6a9f121cfca0b1087f8d05
erramesh000 <erramesh000@gmail.com>
2026-02-01 13:27:33 +0530

    update favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
                     content="Advanced Healing Massage Ltd, Advanced Healing Massage Ltd Calgary AB, Massage Therapy Calgary, Registered Massage Therapist Calgary, Professional Massage Therapy Calgary, Healing Massage Calgary, Massage Therapy in Calgary AB, Massage Therapist near T3N 1V9, Calgary Massage Clinic, Best Massage Therapy Calgary, Massage Services Calgary Alberta, Therapeutic Massage Calgary, Deep Tissue Massage Calgary, Relaxation Massage Calgary, Stress Relief Massage Calgary, Pain Relief Massage Calgary, Sports Massage Calgary, Prenatal Massage Calgary, Swedish Massage Calgary, Affordable massage therapy in Calgary, Professional massage therapist in Calgary Alberta, Book massage appointment in Calgary, Healing and relaxation massage Calgary"
                 />
 
+                <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
 
             </head>
             <body>
